0ed1a142d14caa2fda70e4df04b656b32f0eb3cc
YangYang <yy2473@nyu.edu>
2020-05-29 20:03:59 -0400

    fix

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const changeTitle = ()=> window.document.title = window.document.title.replace(/(\(\d+)\s封私信\)\s/g, '');
+setInterval(changeTitle, 5000);
 setTimeout(changeTitle, 1000);
-changeTitle;
